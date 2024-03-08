95bc52fea20fcb837c0369950e9e7528eb77d880
Siomara Alonso <xiomara281190@gmail.com>
2024-03-08 12:47:48 +0000

    Updated

--- README.md
+++ README.md
@@ -24,10 +24,12 @@ deactivate
 pip install -r requirements.txt
 ```
 
-3. Save images to convert in `images` folder, then run `convert.py` script:
+3. Ensure the `images` folder is present in the root directory of the project.
+
+4. Save images to convert in `images` folder, then run `convert.py` script:
 
 ```bash
 python convert.py
 ```
 
-4. Results will be in `converted_images`.
+5. Results will be in `converted_images`.
--- convert.py
+++ convert.py
@@ -1,15 +1,40 @@
 from PIL import Image
-import pillow_avif
+from typing import Optional
 import os
+import pillow_avif
 
+# Define the image formats and the default paths
 IMAGE_FORMATS = ['.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp', '.avif']
-IMAGES_TO_CONVERT = os.path.abspath('images')
+DEFAULT_SOURCE_FOLDER = os.path.abspath('images')
 CONVERTED_IMAGES = os.path.abspath('converted_images')
 
+# Function to check if the source folder exists
+def check_source_folder(folder: Optional[str]) -> str:
+    # Use the default folder if none is provided
+    if folder is None:
+        folder = DEFAULT_SOURCE_FOLDER
 
-def convert_to_png():
-    for filename in os.listdir(IMAGES_TO_CONVERT):
-        file_path = os.path.join(IMAGES_TO_CONVERT, filename)
+    # If the folder does not exist, print a message and return an empty string
+    if not os.path.exists(folder):
+        print(f"The source folder '{folder}' does not exist.")
+        return None
+    
+    return folder
+
+# Function to convert images to PNG format
+def convert_to_png(source_folder: Optional[str] = None):
+    # Verify and get the source folder
+    images_folder = check_source_folder(source_folder)
+    if images_folder is None:
+        return
+
+    # Create the converted images folder if it doesn't exist
+    if not os.path.exists(CONVERTED_IMAGES):
+        os.makedirs(CONVERTED_IMAGES)
+
+    # Process the images in the source folder
+    for filename in os.listdir(images_folder):
+        file_path = os.path.join(images_folder, filename)
         
         if filename.lower().endswith(tuple(IMAGE_FORMATS)):
             try:
@@ -18,13 +43,11 @@ def convert_to_png():
                         img = img.convert('RGBA')
                         
                     new_filename = os.path.splitext(filename)[0] + '.png'
-                    
                     img.save(os.path.join(CONVERTED_IMAGES, new_filename), 'PNG', optimize=True)
-                    
-                print(f"Converted {filename} to {new_filename}")
-                
+                    print(f"Converted {filename} to {new_filename}")
             except Exception as e:
                 print(f"Error converting {filename}: {e}")
-                
 
+
+# Call the function with the default folder if no path is provided
 convert_to_png()
